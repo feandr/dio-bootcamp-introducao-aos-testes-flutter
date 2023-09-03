a6c44bf5097bef08b44e9939e1ad4b3b73256753
FernandaOA <nanda.oliveir@hotmail.com>
2023-09-02 23:25:05 -0300

    aula dio conhecendo matchers

--- lib/meu_app_testes.dart
+++ lib/meu_app_testes.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 double calcularDesconto(double valor, double desconto, bool percentual) {
 if(valor <= 0) {
   throw ArgumentError("O valor do produto não pode ser zero");
@@ -9,4 +11,12 @@ if(percentual){
 return valor - ((valor * desconto) / 100);
 }
 return valor - desconto;
+}
+
+String convertToUperCase(String texto) {
+  return texto.toUpperCase();
+}
+
+double retornaValor(double valor) {
+  return valor;
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,6 @@ import 'package:flutter_intro_testes/meu_app_testes.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Calcula o valor do produto com desconto sem porcentagem', () {
-    expect(app.calcularDesconto(1000, 150, false), 850);
-  });
-
   group("Calcular com desconto", () {
     var valuesToTest = {
       {'valor': 1000, 'desconto': 150, 'percentual': false}: 850,
@@ -39,4 +35,20 @@ void main() {
       });
     }
   });
+
+  test('Testar conversão para uppercase', () {
+    expect(app.convertToUperCase("dio"), equals("DIO"));
+  }); 
+
+    test('Testar conversão para uppercase 2', () {
+    expect(app.convertToUperCase("dio"), equalsIgnoringCase("dio"));
+  }); 
+
+      test('Testar double', () {
+    expect(app.retornaValor(50.0), greaterThanOrEqualTo(50));
+  }); 
+
+      test('Começa com', () {
+    expect(app.convertToUperCase("dio"), startsWith("D"));
+  }); 
 }
